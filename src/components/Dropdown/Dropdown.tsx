8061ecf9929f72dff07b745b7aa217488fac7b4d
0xnihilist <freemasonga1@gmail.com>
2026-02-06 06:38:15 +0700

    Update

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -259,7 +259,7 @@ export function Dropdown({
   return (
     <div className={cx('relative w-full', className)} ref={ref}>
       {label && (
-        <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1.5 ml-1">
+        <label className="block text-sm font-medium text-gray-900 dark:text-gray-100 mb-1.5 ml-1">
           {label}
         </label>
       )}
